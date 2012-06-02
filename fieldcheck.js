c794ecac452dc6ead0d7cc2a55d07863e204b6f6
Dieter Vanmoortel <vanmoortel@telenet.be>
2012-06-02 21:23:16 +0200

    POC
    still need to figure out if I'm gonna bootstrap or not..

--- fieldcheck.js
+++ fieldcheck.js
@@ -4,24 +4,20 @@
     attach: function(context) {
       if($('[validators]').length) {
         $('[validators]').bind('blur', function(){
-          console.log($(this).attr('validators'));
           element = $(this);
           $.ajax({
-            url: Drupal.settings.basePath + 'sites/all/modules/dev/fieldcheck/fieldcheck.json.php',
+            url: Drupal.settings.basePath + 'sites/all/modules/custom/fieldcheck/fieldcheck.json.php',
             dataType: "json",
             type: "POST",
             data: {
-              id: element.attr('id'),
-              type: 'validate',
-              validate: event.data.validate,
+              validators: element.attr('validators'),
               value: element.val(),
 //              requiredValue: Drupal.behaviors.featureForm.getRequiredValue( event.data.requiredValue )
             },
-            success: function(data)
-            {
-              if( data.status )
-              {
-//                Drupal.behaviors.featureForm.changeFieldStatus(element, 'status-success');
+            success: function(data){
+              console.log(data);
+              if( data.status ) {
+                //                Drupal.behaviors.featureForm.changeFieldStatus(element, 'status-success');
               }
               else {
 //                Drupal.behaviors.featureForm.changeFieldStatus(element, 'status-error', data.error);
