3e45208897dfb929cc1fea1a8243080bfb4387f8
Dieter Vanmoortel <dieter.vanmoortel@nascom.be>
2012-08-22 12:23:39 +0200

    added masked input plugin

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ##Field Check module provides an API for easy and instant form field validation.
 
+##What does it do?
+
+- Instant form validation and feedback through fast AJAX calls
+- Integrated with Drupal's element_validation for forms
+- Optional masked input in fields using the Masked Input Plugin (http://digitalbush.com/projects/masked-input-plugin/)
+- Optional : hide all elements that were succesfully validated after submit ( better UX )
+
 ###How it works :
 
 When building your form, add an array of validators to the attributes of your form item.
@@ -13,10 +20,11 @@ $form['test'] = array(
     '#title' => 'test me',
     '#attributes' => array(
       'validators' => array('required', 'number', 'even'),
+      'mask' = '99/99/9999|..',
     ),
 );
 
-You can use the validators of fieldcheck module, listed below.. or you can write your own validators.
+You can use the validators of fieldcheck module, which will be added in time.. or you can write your own validators.
 
 ###How to write your own validators? => Hook_fieldcheck
 
--- fieldcheck.js
+++ fieldcheck.js
@@ -9,51 +9,61 @@
       }
       // core functionality for validation
       if($('[validators]').length) {
-        $('[validators]').bind('blur', function(){
-          // spinner options : Move to function?
-          var opts = {
-            lines: 5, // The number of lines to draw
-            length: 3, // The length of each line
-            width: 3, // The line thickness
-            radius: 4, // The radius of the inner circle
-            corners: 1, // Corner roundness (0..1)
-            rotate: 0, // The rotation offset
-            color: '#000',// #rgb or #rrggbb
-            speed: 0.7, // Rounds per second
-            trail: 60, // Afterglow percentage
-            shadow: false, // Whether to render a shadow
-            hwaccel: false, // Whether to use hardware acceleration
-            className: 'spinner', // The CSS class to assign to the spinner
-            zIndex: 2e9, // The z-index (defaults to 2000000000)
-            top: 'auto', // Top position relative to parent in px
-            left: 'auto' // Left position relative to parent in px
-          };
-          element = $(this);
-          var spinner = new Spinner(opts).spin();
-          element.parent('.form-item').append(spinner.el);
-          // Exception : required values
-          if(element.val() == '' && element.hasClass('required')){
-            data = new Object();
-            data.succes = false;
-            data.error = Drupal.t('This is a required field');
-            Drupal.behaviors.fieldCheck.changeFieldStatus(element, data);
-          }
-          else if(element.val() != ''){
-            $.ajax({
-              url: Drupal.settings.basePath + Drupal.settings.fieldCheck.modulePath + '/fieldcheck.json.php',
-              dataType: "json",
-              type: "POST",
-              data: {
-                validators: element.attr('validators'),
-                value: element.val()
-              },
-              success: function(data){
-                spinner.stop();
-                Drupal.behaviors.fieldCheck.changeFieldStatus(element, data);
-              }
-            });
-          }
+        $('[validators]').each(function(){
+          $(this).bind('blur', function(){
+            // spinner options : Move to function?
+            var opts = {
+              lines: 5, // The number of lines to draw
+              length: 3, // The length of each line
+              width: 3, // The line thickness
+              radius: 4, // The radius of the inner circle
+              corners: 1, // Corner roundness (0..1)
+              rotate: 0, // The rotation offset
+              color: '#000',// #rgb or #rrggbb
+              speed: 0.7, // Rounds per second
+              trail: 60, // Afterglow percentage
+              shadow: false, // Whether to render a shadow
+              hwaccel: false, // Whether to use hardware acceleration
+              className: 'spinner', // The CSS class to assign to the spinner
+              zIndex: 2e9, // The z-index (defaults to 2000000000)
+              top: 'auto', // Top position relative to parent in px
+              left: 'auto' // Left position relative to parent in px
+            };
+            element = $(this);
+            var spinner = new Spinner(opts).spin();
+            
+            // Exception : required values
+            if(element.val() == '' && element.hasClass('required')){
+              data = new Object();
+              data.succes = false;
+              data.error = Drupal.t('This is a required field');
+              Drupal.behaviors.fieldCheck.changeFieldStatus(element, data);
+            }
+            else if(element.val() != ''){
+              element.parent('.form-item').append(spinner.el);
+              $.ajax({
+                url: Drupal.settings.basePath + Drupal.settings.fieldCheck.modulePath + '/fieldcheck.json.php',
+                dataType: "json",
+                type: "POST",
+                data: {
+                  validators: element.attr('validators'),
+                  value: element.val()
+                },
+                success: function(data){
+                  spinner.stop();
+                  Drupal.behaviors.fieldCheck.changeFieldStatus(element, data);
+                }
+              });
+            }
+          })
         });
+      }
+      if($('[mask]').length) {
+        $('[mask]').each(function(){
+          var mask = $(this).attr('mask');
+          var mask_components = mask.split('|');
+          $(this).mask(mask_components[0],{placeholder:mask_components[1]});
+        })
       }
     },
     changeFieldStatus: function(element, data) {
